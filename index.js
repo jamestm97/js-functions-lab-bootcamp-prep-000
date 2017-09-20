f48bf08364c3ac3d883db765d5e7d5ee66a49e47
James McKay <jamestm97@gmail.com>
2017-09-20 22:47:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountDown(days, holiday) {
-  return `It\'s ${days} days until ${holiday}`
+  return `It\'s ${days} days until ${holiday}!`
 }
